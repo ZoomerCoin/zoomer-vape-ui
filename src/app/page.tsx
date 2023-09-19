025aed92ba4f457c1a4fd96a03e35b0b1c60a192
Rahul Sethuram <rahul.eth@icloud.com>
2023-09-19 22:30:16 +0400

    fix: ui tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 "use client";
 
-import { VStack, Box, Flex, Spacer } from "@chakra-ui/react";
+import {
+  VStack,
+  Box,
+  Flex,
+  Spacer,
+  Button,
+  LinkOverlay,
+  LinkBox,
+  Image,
+} from "@chakra-ui/react";
 import { NavBar } from "../components/NavBar";
 import { GameUI } from "../components/GameUI";
+
+export const BUY_ZOOMER_LINK =
+  "https://app.uniswap.org/#/tokens/ethereum/0x0d505c03d30e65f6e9b4ef88855a47a89e4b7676";
 
 const Page = () => {
   return (
@@ -13,8 +25,24 @@ const Page = () => {
           <Spacer />
           <Box width={640}>
             <GameUI />
+            <LinkBox>
+              <Button
+                width="100%"
+                backgroundColor="black"
+                color="#FEFC52"
+                mt={4}
+              >
+                <LinkOverlay href={BUY_ZOOMER_LINK} isExternal>
+                  BUY ZOOMER
+                </LinkOverlay>
+              </Button>
+            </LinkBox>
           </Box>
           <Spacer />
+        </Flex>
+        <Flex>
+          <Spacer />
+          <Image src="/vaper.webp" alt="take a hit" />
         </Flex>
       </VStack>
     </>
--- src/components/GameUI.tsx
+++ src/components/GameUI.tsx
@@ -49,9 +49,7 @@ import { useAccount, useWalletClient } from "wagmi";
 import { goerli } from "wagmi/chains";
 import { useAddRecentTransaction } from "@rainbow-me/rainbowkit";
 import { useState } from "react";
-
-const BUY_ZOOMER_LINK =
-  "https://app.uniswap.org/#/tokens/ethereum/0x0d505c03d30e65f6e9b4ef88855a47a89e4b7676";
+import { BUY_ZOOMER_LINK } from "../app/page";
 
 export const GameUI = () => {
   const { address } = useAccount();
