cbe40fd04fede16ed977080d1bc0b6322887be35
Rahul Sethuram <rahul.eth@icloud.com>
2023-11-02 17:06:38 +0400

    feat: add alchemy

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,13 +2,16 @@ import { getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { configureChains, createConfig } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
-
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 const walletConnectProjectId = "296a55745d9880bb16e1386b1b0eb360";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet],
-  [publicProvider()]
+  [
+    publicProvider(),
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY_MAINNET! }),
+  ]
 );
 
 const { connectors } = getDefaultWallets({
